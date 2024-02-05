c97409aeffb258246560df7e8ea5dd00fad87192
Nuraiym Nurbulatova <140681537+nooraiym@users.noreply.github.com>
2024-02-05 17:23:25 +0600

    add main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import styled from 'styled-components';
 import { Header } from './assets/layout/header/Header';
+import { Main } from './assets/layout/sections/main/Main';
 
 
 function App() {
     return (
         <div className="App">
             <Header></Header>
+            <Main />
         </div>
     );
 }
---
+++ src/assets/layout/sections/main/Main.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import photo from '../../../img/photo.jpg'
+import { FlexWrapper } from '../../../../components/FlexWrapper';
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={'center'} justify={'space-around'}>
+            <div>
+                <span>Hi There</span>
+                <h2>I am Svetlana Dyablo</h2>
+                <MainTitle>A Web Developer.</MainTitle>
+            </div>
+
+            <Photo src={photo} alt="" />
+            </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #edd5d5;
+`
+const MainTitle = styled.h1`
+    font-family: 'Courier New', Courier, monospace;
+`
+
+const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
