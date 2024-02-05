174dc48108fdc6d5be6359240fba33c7fe8154ad
Nuraiym Nurbulatova <140681537+nooraiym@users.noreply.github.com>
2024-02-05 16:37:21 +0600

    add header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import './App.css';
 import styled from 'styled-components';
+import { Header } from './assets/layout/header/Header';
 
 
 function App() {
     return (
         <div className="App">
-            <Title>Welcome</Title>
+            <Header></Header>
         </div>
     );
 }
 
 export default App;
-
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
